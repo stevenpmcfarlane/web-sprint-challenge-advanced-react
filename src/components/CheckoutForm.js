7ece06ebb79c324791b6d5b0897fe60ded422cec
Steven <stevenpmcfarlane@gmail.com>
2021-02-12 11:00:09 -0700

    first commit

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -15,11 +15,8 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  // const [{plantresdata}, handleChanges, clearForm] = useForm(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+
 import axios from "axios";
+import useForm from '../hooks/useForm'
 
 export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
+  
 
+  // add state with a property called "plants" - initialize as an empty array
+  
+  // axios
+  // .get("http://localhost:3333/plants")
+  // .then((res) => {
+  //   console.log(res);
+  //   set(res.data.//results);
+  // })
+  // .catch((err) => setError(err));
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
